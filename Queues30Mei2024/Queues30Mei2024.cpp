3cbf944f7f62d5132babb9069e699f0ee06e85c1
adilaroisa <adilaroisa688@gmail.com>
2024-05-30 12:51:20 +0700

    membuat void remove

--- Queues30Mei2024/Queues30Mei2024.cpp
+++ Queues30Mei2024/Queues30Mei2024.cpp
@@ -37,7 +37,15 @@ public:
         }
         queue_array[REAR] = num;
     }
-    
+    void remove() {
+        //cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue underflow\n";
+            return;
+        }
+       
+    }
+   
 };
 
 int main()
